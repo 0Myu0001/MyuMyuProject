c6210577acfe8df3722906a0c144b8dbcc1d236e
0Myu0001 <ktksyr193@gmail.com>
2024-05-21 16:32:33 +0900

    Edited basic MainExplorer component also, supported responsive design for MainExplorer component

--- src/components/MainExplorer.jsx
+++ src/components/MainExplorer.jsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import useMediaQuery from '@mui/system/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
 const MainExplorer = () => {
+  const theme = useTheme();
+  const mdmatches = useMediaQuery(theme.breakpoints.up('(min-width:960px)'));
+  const smMatches = useMediaQuery(theme.breakpoints.up('(min-width:450px)'));
+
   return(
     <Paper 
       sx = {{m: '5px', gridArea: '2 / 2 / 25 / 5', backgroundColor: 'rgba(255, 255, 255, 0.1)', borderRadius: '15px',}}
@@ -12,8 +18,67 @@ const MainExplorer = () => {
       <Box sx={{
         height: '100%',
         width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        overflowY: 'auto'
       }}>
-        <Typography variant='h2' sx={{fontFamily: 'Noto Sans JP',}}>Top Weekly</Typography>
+        <Typography variant='h2' sx={{fontFamily: 'Noto Sans JP',}}>Explore</Typography>
+        <Box sx={{display: 'flex', flexDirection: 'column',}}>
+          <Typography variant='h4' sx={{fontFamily: 'Noto Sans JP',}}>Suggest</Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', overflowX: 'auto'}}>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 4 / 3 '}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 4 / 3 '}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 4 / 3 '}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 4 / 3 '}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 4 / 3 '}}>
+            </Paper>
+          </Box>
+        </Box>
+        <Box sx={{display: 'flex', flexDirection: 'column',}}>
+          <Typography variant='h4' sx={{fontFamily: 'Noto Sans JP',}}>Playlists</Typography>
+          <Box sx={{display: 'flex', flexDirection: 'row', overflowX: 'auto', }}>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 '}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 '}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 '}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 '}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 '}}>
+            </Paper>
+          </Box>
+        </Box>
+        <Box sx={{display: 'flex', flexDirection: 'column',}}>
+          <Typography variant='h4' sx={{fontFamily: 'Noto Sans JP',}}>Artists</Typography>
+          <Box sx={{display: 'flex', flexDirection: 'row', overflowX: 'auto', }}>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 ', borderRadius: '80px'}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 ', borderRadius: '80px'}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 ', borderRadius: '80px'}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 ', borderRadius: '80px'}}>
+            </Paper>
+          </Box>
+        </Box>
+        <Box sx={{display: 'flex', flexDirection: 'column',}}>
+          <Typography variant='h4' sx={{fontFamily: 'Noto Sans JP',}}>Singles</Typography>
+          <Box sx={{display: 'flex', flexDirection: 'row', overflowX: 'auto', }}>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 '}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 '}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 '}}>
+            </Paper>
+            <Paper sx={{m: '20px', height: '120px', aspectRatio: ' 1 / 1 '}}>
+            </Paper>
+          </Box>
+        </Box>
       </Box>
     </Paper>
       
--- src/components/MainLibrary.jsx
+++ src/components/MainLibrary.jsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
+import useMediaQuery from '@mui/system/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
 const MainLibrary = () => {
+
+  const theme = useTheme();
+  const mdMatches = useMediaQuery(theme.breakpoints.up('(min-width:960px)'));
+  const smMatches = useMediaQuery(theme.breakpoints.up('(min-width:450px)'));
+
   return(
     <Paper 
       sx = {{m: '5px', gridArea: '2 / 2 / 25 / 5', backgroundColor: 'rgba(255, 255, 255, 0.1)', borderRadius: '15px',}}
@@ -19,7 +26,7 @@ const MainLibrary = () => {
       }}>
         <Typography variant='h2' sx={{fontFamily: 'Noto Sans JP', fontSize: '50px', mx: '10px',}}>Your Library</Typography>
         <Typography variant='h4' sx={{fontFamily: 'Noto Sans JP', fontSize: '32px', mx: '10px',}}>Playlists</Typography>
-        <Box sx={{minHeight: '250px',my: '1%', display: 'flex', flexWrap: 'wrap', flexDirection: 'row', flexWrap: 'nowrap', overflowX: 'auto',}}>
+        <Box sx={{minHeight: '250px',my: '1%', display: 'flex', flexDirection: 'row', flexWrap: 'nowrap', overflowX: 'auto',}}>
           <Paper sx={{minWidth: '180px', pt: '180px', m: '20px'}}></Paper>
           <Paper sx={{minWidth: '180px', pt: '180px', m: '20px'}}></Paper>
           <Paper sx={{minWidth: '180px', pt: '180px', m: '5%'}}></Paper>
