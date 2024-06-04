b2ff1d48b663c4695388d98d9cbeefc012a7ad85
0Myu0001 <ktksyr193@gmail.com>
2024-06-05 07:50:28 +0900

    complete main player and player provider

--- src/index.js
+++ src/index.js
@@ -6,18 +6,22 @@ import App from './App';
 import Home from './home';
 import reportWebVitals from './reportWebVitals';
 import Login from './Login';
+import { PlayerProvider } from './components/PlayerProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/home' element={<Home />}/>
-        <Route path='/signin/*' element={<Login />} />
-        <Route path='/*' element={<App />}/>
-      </Routes>
+      <PlayerProvider>
+        <Routes>
+          <Route path='/home' element={<Home />}/>
+          <Route path='/*' element={<App />}/>
+          <Route path='/signin/*' element={<Login />} />
+        </Routes>
+      </PlayerProvider>
     </BrowserRouter>
+
   </React.StrictMode>
 );
 
