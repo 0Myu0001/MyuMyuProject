4e45d400a5da7fc5708d24b3b34e59b84c500dff
0Myu0001 <ktksyr193@gmail.com>
2024-06-22 12:24:19 +0900

    Trying responsive design with CSS Grid and Flexbox.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useBreakPoints } from './components/BreakPoint.jsx';
 import MenuList from './components/menu.jsx';
 import MainPlayer from './components/MainPlayer.jsx';
 import MainExplorer from './components/MainExplorer.jsx';
 import MainLibrary from './components/MainLibrary.jsx';
+import Playlist from './components/Playlist.jsx';
 import NotificationWindow from './components/NotificationWindow.jsx';
 import AccountPanel from './components/AccountPanel.jsx';
 import Search from './components/Search.jsx';
@@ -11,21 +13,21 @@ import Player from './components/Player.jsx';
 import TrendsBar from './components/TrendsBar.jsx';
 import Box from '@mui/material/Box';
 
-const style = {
-  height: '100vh',
-  width: '100vw',
-  backgroundColor: '#F2F8FF',
-  fontFamily: 'Roboto',
-  color: '#3F3F3F',
-  display: 'grid',
-  gridTemplateColumns: 'repeat(6, 1fr)',
-  gridTemplateRows: 'repeat(24, 1fr)',
-}
-
 const App = () => {
+  const { mdMatches, smMatches } = useBreakPoints();
+
   return (
     <div>
-      <Box sx={style}>
+      <Box sx={{
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: '#F2F8FF',
+        fontFamily: 'Roboto',
+        color: '#3F3F3F',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(6, 1fr)',
+        gridTemplateRows: 'repeat(24, 1fr)',
+      }}>
         <TrendsBar />
         <MenuList />
         <Routes>
@@ -33,6 +35,7 @@ const App = () => {
           <Route path='/library' element={<MainLibrary />} />
           <Route path='/notification' element={<NotificationWindow />} />
           <Route path='/account' element={<AccountPanel/>} />
+          <Route path='/playlist/*' element={<Playlist />} />
         </Routes>
         <Search />
         <Player />
